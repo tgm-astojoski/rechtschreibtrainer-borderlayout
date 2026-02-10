9d83e39645d5ddeb9061178881c3d55601b04df4
astojoski <astojoski@student.tgm.ac.at>
2026-02-10 09:20:45 +0100

    Did some more basics

---
+++ src/model/Fragenpool.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.util.Arrays;
+
+public class Fragenpool {
+    private Frage[] fragen;
+    private String titel;
+
+    public Fragenpool(String titel){
+        setTitel(titel);
+        setFragen(new Frage[0]);
+    }
+
+    public Fragenpool(String titel, Frage[] fragen){
+        setTitel(titel);
+        setFragen(fragen);
+    }
+
+    public void addFrage(Frage question){
+        this.fragen = Arrays.copyOf(fragen,  fragen.length + 1);
+        this.fragen[fragen.length-1] = question;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public Frage[] getFragen() {
+        return fragen;
+    }
+
+    public void setFragen(Frage[] fragen) {
+        this.fragen = fragen;
+    }
+}
--- src/model/WTBLFrageSaveLoad.java
+++ src/model/WTBLFrageSaveLoad.java
@@ -1,4 +1,32 @@
 package model;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WTBLFrageSaveLoad {
+    private String defaultPfad;
+
+    public WTBLFrageSaveLoad(String defaultPfad) {
+        setDefaultPfad(defaultPfad);
+    }
+
+    public void saveQuestion(String path, String filename, Frage question) throws IOException {
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path+"\\"+filename + ".txt")))){
+            bw.write(question.toString());
+        }
+    }
+
+    public String loadQuestion(String filename, String pfad){
+        return "";
+    }
+
+    public String getDefaultPfad() {
+        return defaultPfad;
+    }
+
+    public void setDefaultPfad(String defaultPfad) {
+        this.defaultPfad = defaultPfad;
+    }
 }
