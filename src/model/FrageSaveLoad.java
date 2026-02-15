734ab2bb8fb0136401f1cffaa3366881546e802e
bluka <bluka@student.tgm.ac.at>
2026-02-15 19:11:54 +0100

    FrageSaveLoad Klassen erstellt

--- src/model/FrageSaveLoad.java
+++ src/model/FrageSaveLoad.java
@@ -1,32 +1,82 @@
 package model;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class FrageSaveLoad {
-    private String defaultPfad;
 
-    public FrageSaveLoad(String defaultPfad) {
-        setDefaultPfad(defaultPfad);
-    }
+    private String defaultPfad = "./";
 
-    public void saveQuestion(String path, String filename, Frage question) throws IOException {
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(new File(path+"\\"+filename + ".txt")))){
-            bw.write(question.toString());
+    // ---------------- SAVE ----------------
+
+    public void saveFragenpool(Fragenpool pool, String filename) throws IOException {
+
+        try (PrintWriter outputStream = new PrintWriter(filename + ".txt")) {
+
+            // Titel speichern
+            outputStream.println(pool.getTitel());
+
+            // Trennlinie
+            outputStream.println("++++++++++");
+
+            // Fragen speichern
+            for (Frage f : pool.getFragen()) {
+                if (f != null) {
+                    outputStream.println(f.toString());
+                }
+            }
         }
     }
 
-    public String loadQuestion(String filename, String pfad){
-        return "";
+    public void saveFragenpool(Fragenpool pool, String filename, String pfad) throws IOException {
+        saveFragenpool(pool, pfad + File.separator + filename);
     }
 
-    public String getDefaultPfad() {
-        return defaultPfad;
+    // ---------------- LOAD ----------------
+
+    public Fragenpool loadFragenpool(String filename) throws IOException {
+
+        Fragenpool pool = null;
+
+        try (Scanner inputStream =
+                     new Scanner(new BufferedReader(new FileReader(filename + ".txt")))) {
+
+            if (!inputStream.hasNextLine()) {
+                return null;
+            }
+
+            // Titel lesen
+            String titel = inputStream.nextLine();
+            pool = new Fragenpool(titel);
+
+            while (inputStream.hasNextLine()) {
+
+                String line = inputStream.nextLine();
+
+                if (line.equals("++++++++++") || line.trim().isEmpty()) {
+                    continue;
+                }
+
+                Scanner lineScanner = new Scanner(line);
+                lineScanner.useDelimiter("#");
+
+                int imageFlag = Integer.parseInt(lineScanner.next());
+                String question = lineScanner.next();
+                String answer = lineScanner.next();
+
+                boolean image = (imageFlag == 0);
+
+                Frage frage = new Frage(question, answer, image);
+                pool.addFrage(frage);
+
+                lineScanner.close();
+            }
+        }
+
+        return pool;
     }
 
-    public void setDefaultPfad(String defaultPfad) {
-        this.defaultPfad = defaultPfad;
+    public Fragenpool loadFragenpool(String filename, String pfad) throws IOException {
+        return loadFragenpool(pfad + File.separator + filename);
     }
 }
