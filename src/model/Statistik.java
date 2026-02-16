0f2f90fd513d144f58df0ee93ab8f0c08757769b
bluka <bluka@student.tgm.ac.at>
2026-02-16 14:05:39 +0100

    Statistik Klassen erstellt

--- src/model/Statistik.java
+++ src/model/Statistik.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Statistik {
+
+    private int richtigeFragen;
+    private int falscheFragen;
+
+    public Statistik() {
+        richtigeFragen = 0;
+        falscheFragen = 0;
+    }
+
+    public boolean pruefeAntwort(Frage frage, String benutzerAntwort) {
+
+        if (frage.getAnswer().equals(benutzerAntwort)) {
+            richtigeFragen++;
+            return true;
+        } else {
+            falscheFragen++;
+            return false;
+        }
+    }
+
+    public int getRichtigeFragen() {
+        return richtigeFragen;
+    }
+
+    public int getFalscheFragen() {
+        return falscheFragen;
+    }
+
+    public int getGesamt() {
+        return richtigeFragen + falscheFragen;
+    }
 }
